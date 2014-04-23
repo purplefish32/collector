52386962117d1d4639c30a4ba951a2d9ac5f84c2
Donovan Tengblad <purplefish32@gmail.com>
2014-04-23 16:44:00 +0200

    Aliasses

---
+++ app/config/aliases.php
@@ -0,0 +1,16 @@
+<?php
+
+$config['aliases'] = array(
+    'Slim'      => 'Slim\Slim',
+    'Middleware'=> 'Slim\Middleware',
+    'Model'     => 'Illuminate\Database\Eloquent\Model',
+    'Sentry'    => 'Cartalyst\Sentry\Facades\Native\Sentry',
+    'App'       => 'SlimFacades\App',
+    'Config'    => 'SlimFacades\Config',
+    'Input'     => 'SlimFacades\Input',
+    'Log'       => 'SlimFacades\Log',
+    'Request'   => 'SlimFacades\Request',
+    'Response'  => 'SlimFacades\Response',
+    'Route'     => 'SlimFacades\Route',
+    'View'      => 'SlimFacades\View',
+);
